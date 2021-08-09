27e8d52b4e113d0285c7078e0367672a448bd39d
I2rys <i2rys@protonmail.com>
2021-08-08 18:42:45 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+//Dependencies
+const Dcipher = require("dcipher")
+const Chalk = require("chalk")
+
+//Variables
+const Self_Args = process.argv.slice(2)
+
+//Main
+if(Self_Args.length == 0){
+    console.log(Chalk.blueBright(`node index.js <MD5/SHA1/SHA224/SHA256/SHA384/SHA512/RIPEMD320>
+Example: node index.js f4020e91252aafd4b18d8acd17f883db`))
+    process.exit()
+}
+
+Main()
+function Main(){
+    Dcipher(Self_Args[0]).then(result =>{
+        if(result.indexOf("not") != -1){
+            console.log(Chalk.redBright("Hash type not supported."))
+            process.exit()
+        }
+
+        console.log(Chalk.greenBright(`Decrypted ${Self_Args[0]} result is ${result}`))
+    })
+}
