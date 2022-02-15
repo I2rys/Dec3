5308157a73d97bac9d5e1eacb63bb2653c500e6d
Trac3D1y <i2rys@protonmail.com>
2022-02-15 20:49:53 +0800

    Optimised

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ const Chalk = require("chalk")
 const Self_Args = process.argv.slice(2)
 
 //Main
-if(Self_Args.length == 0){
+if(!Self_Args.length){
     console.log(Chalk.blueBright(`node index.js <MD5/SHA1/SHA224/SHA256/SHA384/SHA512/RIPEMD320>
 Example: node index.js f4020e91252aafd4b18d8acd17f883db`))
     process.exit()
 }
 
-Main()
-function Main(){
+void function Main(){
     Dcipher(Self_Args[0]).then(result =>{
-        if(result.indexOf("not") != -1){
+        if(result.indexOf("not") !== -1){
             console.log(Chalk.redBright("Hash type not supported."))
             process.exit()
         }
 
         console.log(Chalk.greenBright(`Decrypted ${Self_Args[0]} result is ${result}`))
     })
-}
+}()
